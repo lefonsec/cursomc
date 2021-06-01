c9b76b9068c8639b98dce074e0a90134da75f1c7
Leonardo Fonseca <leofonseca.magalhaes@gmail.com>
2021-06-01 16:23:44 -0300

    Estado,Cidade

--- src/main/java/com/curso/TestConfig.java
+++ src/main/java/com/curso/TestConfig.java
@@ -8,8 +8,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.curso.domain.Categoria;
+import com.curso.domain.Cidade;
+import com.curso.domain.Estado;
 import com.curso.domain.Produto;
 import com.curso.repositories.CategoriaRepository;
+import com.curso.repositories.CidadeRepository;
+import com.curso.repositories.EstadoRepository;
 import com.curso.repositories.ProdutoRepository;
 
 @Configuration
@@ -21,6 +25,12 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -41,6 +51,19 @@ public class TestConfig implements CommandLineRunner {
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null,"Minas Gerais");
+		Estado est2 = new Estado(null,"São Paulo");
+		
+		Cidade cid1 = new Cidade(null,"Uberlândia", est1);
+		Cidade cid2 = new Cidade(null,"São Paulo", est2);
+		Cidade cid3 = new Cidade(null,"Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(cid1));
+		est2.getCidades().addAll(Arrays.asList(cid2,cid3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(cid1,cid2,cid3));
 		
 	}
 
