c767a69d893ecf54b77f9f5cd3e2788031ca3ea9
Leonardo Fonseca <leofonseca.magalhaes@gmail.com>
2021-06-09 14:21:48 -0300

    CategoriaDTO

--- src/main/java/com/curso/resource/CategoriaResource.java
+++ src/main/java/com/curso/resource/CategoriaResource.java
@@ -1,13 +1,23 @@
 package com.curso.resource;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.curso.domain.Categoria;
+import com.curso.dto.CategoriaDTO;
 import com.curso.servicies.CategoriaService;
 
 @RestController
@@ -16,10 +26,35 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaService catService;
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Categoria> findById(@PathVariable Long id){
+	public ResponseEntity<Categoria> findById(@PathVariable Long id) {
 		Categoria obj = catService.finById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+		List<Categoria> lista = catService.findAll();
+		List<CategoriaDTO>listaDto =lista.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listaDto);
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
+		obj = catService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Categoria> upadate(@PathVariable Long id, @RequestBody Categoria obj){
+		obj = catService.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		catService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/curso/servicies/CategoriaService.java
+++ src/main/java/com/curso/servicies/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.curso.servicies;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +14,33 @@ public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository catRepository;
-	
+
 	public Categoria finById(Long id) {
 		Optional<Categoria> obj = catRepository.findById(id);
 		return obj.get();
+	}
+
+	public List<Categoria> findAll() {
+		return catRepository.findAll();
+	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return catRepository.save(obj);
+	}
+
+	public Categoria update(Long id, Categoria obj) {
+		Categoria entity = catRepository.getById(id);
+		update(entity, obj);
+		return catRepository.save(entity);
+	}
+
+	private void update(Categoria entity, Categoria obj) {
+		entity.setNome(obj.getNome());
+
+	}
+
+	public void delete(Long id) {
+		catRepository.deleteById(id);
 	}
 }
